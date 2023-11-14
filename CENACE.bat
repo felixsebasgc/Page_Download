5c4e108daaa318e69d8f8370598f5ad9d32538df
FGonzalez <felixsebasgc@users.noreply.github.com>
2023-11-13 22:56:58 -0500

    Time correction and Wget silent
    This change corrects an error with the folder name and reduces the output from WGET.

--- CENACE.bat
+++ CENACE.bat
@@ -3,15 +3,25 @@
 	::Version antigua para Chrome, no descarga la página.
 		:: "C:\Program Files\Google\Chrome\Application\chrome.exe" --headless --disable-gpu --screenshot=C:\Users\felix\Downloads\%date%.png --window-size=1920,1080 ----virtual-time-budget=5000 https://www.cenace.gob.ec/info-operativa/InformacionOperativa_archivos/sheet004.htm
 
-::Para el funcionamiento de este programa se debe instalar WGET
-"C:\Program Files (x86)\GnuWin32\bin\wget.exe" wget -E -H -k -r -p "C:\Users\felix\Downloads\CENACE_PROYECTO" --no-check-certificate https://www.cenace.gob.ec/info-operativa/InformacionOperativa.htm
+echo Espere, obteniendo datos de CENACE...
+
+::Para el funcionamiento de este programa se debe instalar WGET, -q para completo silencio
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" wget -r -q -P "C:\Users\felix\Downloads\CENACE_PROYECTO" --no-check-certificate https://www.cenace.gob.ec/info-operativa/InformacionOperativa.htm
+
+::"C:\Program Files (x86)\GnuWin32\bin\wget.exe" wget -r -P "C:\Users\felix\Downloads\CENACE_PROYECTO" --no-check-certificate https://www.cenace.gob.ec/info-operativa/InformacionOperativa.htm
+
+::Nombre del archivo
+set filename=%DATE%_%time:~0,2%-%time:~3,2%>nul
 
 ::Copia del archivo a la carpeta de almacenamiento
-mkdir "C:\Users\felix\Downloads\CENACE_PROYECTO\DATOS_ORGANIZADOS\%DATE%_%time::=-%"
-xcopy /s "C:\Users\felix\Downloads\CENACE_PROYECTO\www.cenace.gob.ec\info-operativa" "C:\Users\felix\Downloads\CENACE_PROYECTO\DATOS_ORGANIZADOS\%DATE%_%time::=-%"
+mkdir "C:\Users\felix\Downloads\CENACE_PROYECTO\DATOS_ORGANIZADOS\%filename%">nul 
+xcopy /s "C:\Users\felix\Downloads\CENACE_PROYECTO\www.cenace.gob.ec\info-operativa" "C:\Users\felix\Downloads\CENACE_PROYECTO\DATOS_ORGANIZADOS\%filename%">nul
+
 	::Renombra al archivo
 	::ren "C:\Users\felix\Downloads\CENACE_PROYECTO\DATOS_ORGANIZADOS\InformacionOperativa.htm" "%DATE%_%time::=-%.htm"
 	::ren "C:\Users\felix\Downloads\CENACE_PROYECTO\DATOS_ORGANIZADOS\InformacionOperativa_archivos" "%DATE%_%time::=-%"
+
+echo FIN
 
 
 exit'
